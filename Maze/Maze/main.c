e89f21bffa1e93008ed76562dee4b8c05391c00d
Gonagi <kkh10002@naver.com>
2022-04-12 16:18:32 +0900

    Print_maze함수 수정 및 Textcolor함수 수정

--- Maze/Maze/main.c
+++ Maze/Maze/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <Windows.h>
 #include "pos.h"
 #include "stack.h"
-#include "Windows.h"
 
 #define MAX 4
-#define PATH 0			// 지나갈 수 있는 위치
-#define WALL 1			// 지나갈 수 없는 위치
-#define vISITED 2		// 이미 방문한 위치
-#define BACKTRACKED 3	// 방문했다가 되돌아 나온 위치
+#define PATH 0			// 지나갈 수 있는 위치			// 초록
+#define WALL 1			// 지나갈 수 없는 위치			// 빨강
+#define vISITED 2		// 이미 방문한 위치				// 노랑
+#define BACKTRACKED 3	// 방문했다가 되돌아 나온 위치		// 파랑
 
 int Maze[MAX][MAX];
 int Size;	// 미로의 크기
@@ -15,7 +15,7 @@ int Size;	// 미로의 크기
 void Read_maze();
 void Print_maze();
 void Textcolor(int);	// 출력 글자 색 변경
-enum ColorType { WHITE = 15, BLUE = 9, GREEN = 10, YELLOW = 14 }COLOR;	// 흰 파 초 노
+enum ColorType { RED = 4*16, BLUE = 9*16, GREEN = 10*16, YELLOW = 14*16 }COLOR;	// 흰 파 초 노
 
 int main()
 {
@@ -44,13 +44,21 @@ void Read_maze()
 void Print_maze()
 {
 	for (int i = 0; i < MAX; i++) {
-		for (int j = 0; j < MAX; j++)
-			printf("%d ", Maze[i][j]);
+		for (int j = 0; j < MAX; j++) {
+			switch (Maze[i][j]) {
+			case 0: Textcolor(GREEN); printf("□"); break;
+			case 1: Textcolor(RED); printf("□"); break;
+			case 2: Textcolor(YELLOW); printf(" "); break;
+			case 3: Textcolor(BLUE); printf(" "); break;
+			}
+			// printf("%d ", Maze[i][j]);
+		}
 		printf("\n");
 	}
 }
 
 void Textcolor(int colorNum)
 {
-	SetConsoleActiveScreenBuffer(GetStdHandle(STD_OUTPUT_HANDLE), colorNum);
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorNum);
+	//SetConsoleActiveScreenBuffer(GetStdHandle(STD_OUTPUT_HANDLE), colorNum);
 }
